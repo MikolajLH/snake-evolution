8f0a2e6942720b66fba866ab359df2d9bf0229cc
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-24 23:45:18 +0200

    outline of the Game class

---
+++ game/game.py
@@ -0,0 +1,21 @@
+
+
+class Game:
+    '''
+    Game class the rules of the the game
+    the game is a series of game's state that ends in the termianl state
+    '''
+    def __init__():
+        pass
+
+    def create_initial_state(self) -> state:
+        pass
+        
+    def get_actions(self, state):
+        pass
+
+    def take_action(self, state, action) -> state:
+        pass
+
+    def is_state_terminal(self, state) -> bool:
+        pass
